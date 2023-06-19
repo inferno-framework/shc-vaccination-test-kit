ae85db25ea03beb50eb4554e5143f4408e2efdcb
Karl Naden <knaden@mitre.org>
2023-06-19 16:08:03 -0400

    FI-1954: FHIR is SHC suite description

--- lib/shc_vaccination_test_kit.rb
+++ lib/shc_vaccination_test_kit.rb
@@ -6,6 +6,10 @@ module Covid19VCI
   class Suite < Inferno::TestSuite
     id 'c19-vci'
     title 'SMART Health Cards: Vaccination & Testing'
+    description %(
+      This test suite evaluates the ability of a system to provide
+        access to [SMART Health Cards](https://smarthealth.cards/) via file download or HL7® FHIR® API.
+    )
 
     group from: :vci_file_download
     group from: :vci_fhir_operation
