00e778f3188f54d186ad59493ef336d397ea8bec
Dylan Hall <dehall@mitre.org>
2024-05-03 15:44:08 -0400

    FI-2429: migrate to HL7 validator wrapper

--- lib/shc_vaccination_test_kit.rb
+++ lib/shc_vaccination_test_kit.rb
@@ -11,6 +11,18 @@ module Covid19VCI
       access to [SMART Health Cards](https://smarthealth.cards/) via file download or HL7® FHIR® API.
     )
 
+    VALIDATION_MESSAGE_FILTERS = [
+      /\A\S+: \S+: URL value '.*' does not resolve/,
+    ].freeze
+
+    fhir_resource_validator do
+      igs 'igs/hl7.fhir.uv.smarthealthcards-vaccination-0.5.0-rc.tgz'
+
+      exclude_message do |message|
+        VALIDATION_MESSAGE_FILTERS.any? { |filter| filter.match? message.message }
+      end
+    end
+
     group from: :vci_file_download
     group from: :vci_fhir_operation
   end
