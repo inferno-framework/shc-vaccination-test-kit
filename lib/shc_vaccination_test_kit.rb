9a4c562ed827648ae9e55d037b8e8e2085123a8c
Brian Meshell <bmeshell@mitre.org>
2025-01-13 10:12:19 -0500

    Added shc_qr_code_group and supporting files (views and javascript folders.) Updated routes (ie. file paths) as needed.

--- lib/shc_vaccination_test_kit.rb
+++ lib/shc_vaccination_test_kit.rb
@@ -24,9 +24,30 @@ module Covid19VCI
       end
     end
 
+    scan_qr_code_html = File.read(File.join(__dir__, './covid19_vci/views/scan_qr_code.html'))
+    scan_qr_code_html_route_handler = proc { [200, { 'Content-Type' => 'text/html' }, [scan_qr_code_html]] }
+    route(:get, '/scan_qr_code', scan_qr_code_html_route_handler)
+
+    qr_scanner = File.read(File.join(__dir__, './covid19_vci/javascript/qr-scanner.min.js'))
+    qr_scanner_route_handler = proc { [200, { 'Content-Type' => 'text/javascript' }, [qr_scanner]] }
+    route(:get, '/qr-scanner.min.js', qr_scanner_route_handler)
+
+    qr_scanner_worker = File.read(File.join(__dir__, './covid19_vci/javascript/qr-scanner-worker.min.js'))
+    qr_scanner_worker_route_handler = proc { [200, { 'Content-Type' => 'text/javascript' }, [qr_scanner_worker]] }
+    route(:get, '/qr-scanner-worker.min.js', qr_scanner_worker_route_handler)
+
+    js_qr = File.read(File.join(__dir__, './covid19_vci/javascript/jsQR.js'))
+    js_qr_route_handler = proc { [200, { 'Content-Type' => 'text/javascript' }, [js_qr]] }
+    route(:get, '/jsqr.js', js_qr_route_handler)
+
+    upload_html = File.read(File.join(__dir__, './covid19_vci/views/upload_qr_code.html'))
+    upload_html_route_handler = proc { [200, { 'Content-Type' => 'text/html' }, [upload_html]] }
+    route(:get, '/upload_qr_code', upload_html_route_handler)
+
+
     # Tests and TestGroups
     group from: :shc_file_download_group
     group from: :shc_fhir_operation_group
-
+    group from: :shc_qr_code_group
   end
 end
