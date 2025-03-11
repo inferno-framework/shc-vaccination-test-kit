e271400b6b8886a569dae03c73b64599a9f83763
Yunwei Wang <>
2025-03-11 09:45:15 -0500

    fix last_updated date

--- lib/shc_vaccination_test_kit/version.rb
+++ lib/shc_vaccination_test_kit/version.rb
@@ -1,4 +1,4 @@
 module SHCVaccinationTestKit
   VERSION = '0.4.0'.freeze
-  LAST_UPDATED = '2025-03-10'.freeze # TODO: update next release
+  LAST_UPDATED = '2025-03-11'.freeze # TODO: update next release
 end
